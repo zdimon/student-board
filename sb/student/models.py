c7097f54581adbfc20aed53a8dab5beb25346b4f
Your Name <you@example.com>
2021-09-08 16:21:21 +0300

    commit from zdimon77@gmail.com ----

---
+++ sb/student/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Student(User):
+    fname = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='student_images')
+    account = models.IntegerField()
+    phone = models.CharField(max_length=100)
+
