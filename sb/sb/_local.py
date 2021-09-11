40781928d54bf11fe219d6d99784a6f56a9767fe
Your Name <you@example.com>
2021-09-11 14:13:28 +0300

    commit from zdimon77@gmail.com ----

---
+++ sb/sb/_local.py
@@ -0,0 +1,14 @@
+DATA_DIR='/home/zdimon/Desktop/students/student-board/init_data'
+ALL_FREE=True 
+VIDEO_DIR = '/home/zdimon/Videos/course-data'
+DOMAIN='http://localhost:8001'
+
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+'''
--- sb/sb/settings.py
+++ sb/sb/settings.py
@@ -83,12 +83,6 @@ WSGI_APPLICATION = 'sb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
