3937c5e9fa5aa65caebbd343a0347c53dc192f1e
Your Name <you@example.com>
2021-09-12 10:56:47 +0300

    auto

--- sb/sb/_local.py
+++ sb/sb/_local.py
@@ -17,3 +17,16 @@ DATABASES = {
 }
 
 '''
+
+''' 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'pressa',                                   # Or path to database file if using sqlite3.
+        'USER': 'root',                                   # Not used with sqlite3.
+        'PASSWORD': '1q2w3e',                             # Not used with sqlite3.
+        'HOST': 'localhost',                              # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                                         # Set to empty string for default. Not used with sqlite3.
+    }
+}
+'''
