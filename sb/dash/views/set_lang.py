b70ecbef53de4f3cbe88a71d6a990d3ef1de4ea0
Your Name <you@example.com>
2021-09-12 14:02:59 +0300

    auto

--- sb/dash/views/set_lang.py
+++ sb/dash/views/set_lang.py
@@ -1,6 +1,7 @@
 from django.utils import translation
 from django.shortcuts import redirect
 
+
 def set_lang(request,lang):
     translation.activate(lang)
     return redirect('/')
--- sb/sb/urls.py
+++ sb/sb/urls.py
@@ -21,6 +21,7 @@ from student.views import registration
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.conf.urls.i18n import i18n_patterns
 
 schema_view = get_schema_view(
    openapi.Info(title="Student board API",
@@ -39,9 +40,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('', include('dash.urls')),
-    path('mentor/', include('mentor.urls')),
-    path('student/', include('student.urls')),
+
+
     path('admin/', admin.site.urls),
     path('api', schema_view.with_ui('swagger', cache_timeout=0), 
     name='schema-swagger-ui'),
@@ -52,6 +52,12 @@ urlpatterns = [
     ])),
 
 ]
+
+urlpatterns += i18n_patterns(
+    path('', include('dash.urls')),
+    path('mentor/', include('mentor.urls')),
+    path('student/', include('student.urls')),
+)
 
 from django.conf import settings
 from django.conf.urls.static import static
