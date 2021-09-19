273403248571cd5585669b543ca160c0839f8770
Your Name <you@example.com>
2021-09-19 13:27:50 +0300

    auto

--- sb/course/models.py
+++ sb/course/models.py
@@ -43,6 +43,11 @@ class Course(models.Model):
         return Lesson.objects.filter(course=self).count()
 
     @property
+    def count_labs(self):
+        from course.models import Lab
+        return Lab.objects.filter(course=self).count()
+
+    @property
     def mentors(self):
         from mentor.models import Mentor2Course
         mts = Mentor2Course.objects.filter(course=self, is_approved=True)
