4da8c2d51e8af5bad9f432fd9c86b69df26d23a0
Your Name <you@example.com>
2021-09-19 15:22:09 +0300

    auto

--- react-app/src/components/menu/StudentMenu.jsx
+++ react-app/src/components/menu/StudentMenu.jsx
@@ -2,18 +2,20 @@ import React, { useEffect } from 'react';
 // import PropTypes from 'prop-types';
 import { Request } from '../../Request';
 
-// const getLang = function () {
-//   const url = window.location.href;
-//   console.log(url);
-//   const arr = url.split('/')[3];
-//   return arr;
-// };
+const getLang = function () {
+  const url = window.location.href;
+  console.log(url);
+  const arr = url.split('/')[3];
+  return arr;
+};
 
 export default function StudentMenu() {
   const [courses, setCourses] = React.useState([]);
+  const [lang, setLang] = React.useState('');
 
   useEffect(() => {
     const req = new Request();
+    setLang(getLang());
 
     req
       .get('course/list')
@@ -29,7 +31,7 @@ export default function StudentMenu() {
         <a
           key={el.id}
           className="nav-link collapsed"
-          href={el.get_student_absolute_url}
+          href={`/${lang}/student/detail/course/${el.id}`}
         >
           <i className="fas fa-fw fa-list-alt"></i>
           <span>{el.name}</span>
