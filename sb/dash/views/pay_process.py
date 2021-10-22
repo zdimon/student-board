6a287d8c7db9d291f554df6fb707315f3b2454bc
Your Name <you@example.com>
2021-10-22 13:29:25 +0300

    auto

--- sb/dash/views/pay_process.py
+++ sb/dash/views/pay_process.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from liqpay.liqpay3 import LiqPay
 from sb.settings import LIQPAY_PRIVATE_KEY, LIQPAY_PUBLIC_KEY
 from django.views.decorators.csrf import csrf_exempt
+from student.models import Student2Course, StudentPayment
+
 
 @csrf_exempt
 def pay_process(request):
@@ -12,5 +14,8 @@ def pay_process(request):
     if sign == signature:
         print('callback is valid')
     response = liqpay.decode_data_from_str(data)
+    order = StudentPayment.objects.get(pk=int(response['order_id']))
+    order.is_done = True
+    order.save()
     print('callback data', response)
     return render(request, 'dash/pay_success.html')
