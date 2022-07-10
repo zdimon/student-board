6ee6cc634c4a280babefd242973b6ee0db7ac321
zdimon <zdimon77@gmail.com>
2022-07-10 16:40:57 +0300

    ggg

--- sb/dash/views/index.py
+++ sb/dash/views/index.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from course.models import parse_md
 from sb.settings import DATA_DIR
 from course.models import Course, Kursak
+from django.utils import translation
 
 def index(request):
     courses = Course.objects.filter(is_active=True)
     # kursaks = Kursak.objects.all()
-    path = DATA_DIR+f'/index_ru.md'
-    #f = open(path, 'r')
-    #txt = f.read()
-    #f.close()
-    #index = parse_md(txt)    
-    return render(request, 'dash/index.html', {"courses": courses})
+    path = DATA_DIR+f'/about-{translation.get_language()}.md'
+    f = open(path, 'r')
+    txt = f.read()
+    f.close()
+    about = parse_md(txt)    
+    return render(request, 'dash/index.html', {"courses": courses, "about": about})
--- sb/sb/settings.py
+++ sb/sb/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'uk'
 
 LANGUAGES = [
     ('en','English'),
