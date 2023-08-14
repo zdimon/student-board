f945f1d91d38938ad47fd0a909de53b1300d6a1e
Dima Zharikov <zdimon77@gmail.com>
2023-08-14 19:30:54 +0300

    autocommit

--- sb/dash/views/set_lang.py
+++ sb/dash/views/set_lang.py
@@ -2,15 +2,19 @@ from django.utils import translation
 from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
 from sb.local import DOMAIN
+from django.utils.translation import get_language
 
 def set_lang(request,lang):
     translation.activate(lang)
     next = request.GET.get('next', None)
 
-    if not next:
-        next = '/'
-    else:
-        tmp = next.split('/')[2:]
-        tmp.insert(0,lang)
-        next = '/'.join(tmp)
+    # if not next:
+    #     next = '/'
+    # else:
+    #     tmp = next.split('/')[2:]
+    #     tmp.insert(0,lang)
+    #     next = '/'.join(tmp)
+    #     next = next.replace('-'+get_language(),lang)
+        
+    next = '/'+lang
     return HttpResponseRedirect('%s/%s' %  (DOMAIN,next))
